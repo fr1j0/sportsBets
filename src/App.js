ea55b25ed9b9c2c3f5fe225776b59bd503b5ded9
fr1j0 <fercasti@gmail.com>
2019-04-21 16:02:27 +0200

    Header moved to App level

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import store from './store';
 import Competitions from "./components/pages/competitions";
 import {Provider} from 'react-redux'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Header from "./components/molecules/header";
 
 type Props = {};
 
@@ -13,6 +14,7 @@ class App extends Component<Props> {
     render() {
         return (
             <Provider store={store}>
+                <Header/>
                 <Router>
                     <Route path="/" component={Competitions}/>
                 </Router>
--- src/components/pages/competitions.jsx
+++ src/components/pages/competitions.jsx
@@ -3,7 +3,6 @@ import {connect} from 'react-redux'
 import CompetitionsTemplate from "../templates/competitions";
 import store from "../../store";
 import {fetchCompetitions} from "../../actions/competitions";
-import Header from "../molecules/header";
 
 class Competitions extends Component {
     componentDidMount(): void {
@@ -11,10 +10,7 @@ class Competitions extends Component {
     }
 
     render() {
-        return <Fragment>
-            <Header/>
-            <CompetitionsTemplate competitions={this.props.competitions}/>
-        </Fragment>
+        return <CompetitionsTemplate competitions={this.props.competitions}/>
     }
 }
 
